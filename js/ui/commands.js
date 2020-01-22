24136b3c2ff1483237f7dcf144f97eafb64e2479
Rualark <rualark@gmail.com>
2020-01-22 19:27:52 +0300

    Show keyboard shortcuts

--- js/ui/commands.js
+++ js/ui/commands.js
@@ -76,7 +76,11 @@ export function init_commands() {
     if (!command.id) continue;
     let tooltip = '';
     if (!mobileOrTablet) {
-      tooltip = `data-toggle=tooltip data-container=body data-bondary=window data-placement=bottom title="${command.name}"`;
+      let title = command.name;
+      if (command.keys != null && command.keys.length) {
+        title += '<br>(shortcut ' + command.keys[0] + ')';
+      }
+      tooltip = `data-toggle=tooltip data-html=true data-container=body data-bondary=window data-placement=bottom title="${title}"`;
     }
     st += `<a ${tooltip} id='${command.id}' class='btn btn-outline-white ${command.toolbar.disabled ? "disabled " : ""}p-1' href=# role='button' style='min-width: ${mobileOpt[mobileOrTablet].toolbar_button_width}px; font-size: ${command.toolbar.fontSize * mobileOpt[mobileOrTablet].toolbar_font_scale || '1'}em'>`;
     if (command.toolbar.type === 'image') {
