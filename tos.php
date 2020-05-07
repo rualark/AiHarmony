73c4a723628ff1b92f9a32da9fa5a06fda0552b5
Rualark <rualark@gmail.com>
2020-05-08 01:07:49 +0300

    Remove unneeded php functions

--- tos.php
+++ tos.php
@@ -1,13 +1,9 @@
 <?php
 require_once "lib/mlib.php";
 require_once "lib/config.php";
-require_once "lib/auth.php";
-require_once "lib/reports.php";
-require_once "lib/ajax.php";
 
 $title = "$site_name: Terms and Conditions";
 
-login();
 
 require_once "template/menu.php";
 
@@ -15,5 +11,3 @@ echo "<div class=container>";
 
 require_once "template/tos.php";
 require_once "template/footer.php";
-
-?>
