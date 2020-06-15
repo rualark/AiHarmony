ca36b6ff338dacdc4c1d75313f539b0090828f2d
Rualark <rualark@gmail.com>
2020-06-16 00:23:56 +0300

    CA3: Disable non-permitted first harmony if there is no cantus.

--- js/analysis/modules/CA3.js
+++ js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5391360,
+    STACK_BASE = 5391376,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 148480,
-    DYNAMIC_BASE = 5391360,
-    DYNAMICTOP_PTR = 148320;
+    STACK_MAX = 148496,
+    DYNAMIC_BASE = 5391376,
+    DYNAMICTOP_PTR = 148336;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 147456;
+// STATICTOP = STATIC_BASE + 147472;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 148320;
+      return 148336;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
