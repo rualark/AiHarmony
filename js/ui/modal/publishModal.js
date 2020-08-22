1370bd76a668bdf2421fa9d3dab7c00713c837f3
Rualark <rualark@gmail.com>
2020-08-22 19:30:06 +0300

    Update state encoding version

---
+++ js/ui/modal/publishModal.js
@@ -0,0 +1,43 @@
+import {state} from "../../abc/abchelper.js";
+import { showModal } from "../lib/modal.js";
+import { publish } from "../../integration/publish.js";
+import { mgen_login } from "../../core/remote.js";
+import { nd } from "../../notes/NotesData.js";
+
+function showSelectSecurity() {
+  let st = '';
+  st += `<div class="form-group">`;
+  st += `<label for="sel_security"><b>Allow access to this exercise:</b></label>`;
+  st += `<select class="form-control custom-select" id=sel_security name=sel_security>`;
+  st += `<option value=0>To anyone</option>`;
+  st += `<option value=1 selected>To all authenticated users</option>`;
+  st += `<option value=2>To administrators only</option>`;
+  st += `<option value=3>To me only</option>`;
+  st += `</select>`;
+  st += `</div>`;
+  return st;
+}
+
+export function showPublishModal() {
+  if (state.state !== 'ready') return;
+  let st = '';
+  st += "<b>Publish as:</b> "
+  if (mgen_login !== '') {
+    st += mgen_login;
+  } else {
+    st += `<a href=https://artinfuser.com/counterpoint/login.php target=_blank>Login</a>`;
+  }
+  if (nd.root_eid) {
+    st += "<br><b>Parent exercise:</b> "
+    st += `<a href=https://artinfuser.com/counterpoint/exercise.php?id=${nd.root_eid} target=_blank>#${nd.root_eid}</a>`;
+  }
+  st += showSelectSecurity();
+  let footer = '';
+  footer += `<button type="button" class="btn btn-primary" id=modalOk>OK</button>`;
+  footer += `<button type="button" class="btn btn-secondary" data-dismiss="modal" id=modalCancel>Cancel</button>`;
+  showModal(1, 'Publish exercise to ArtInfuser database', st, footer, [], [], true, ()=>{}, ()=>{});
+  $('#modalOk').click(() => {
+    $('#Modal1').modal('hide');
+    publish(Number($("#sel_security option:selected").val()));
+  });
+}
