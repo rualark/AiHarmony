0a134579c9cb8075b42a22246edab65ea0c31b5a
Rualark <rualark@gmail.com>
2020-09-27 21:04:13 +0300

    CA3: Disable doubling a tritone as a separate mistake, because it just duplicates harmonic tritone mistake.

--- docs/prod/js/analysis/modules/CA3.js
+++ docs/prod/js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5395696,
+    STACK_BASE = 5396032,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 152816,
-    DYNAMIC_BASE = 5395696,
-    DYNAMICTOP_PTR = 152656;
+    STACK_MAX = 153152,
+    DYNAMIC_BASE = 5396032,
+    DYNAMICTOP_PTR = 152992;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 151792;
+// STATICTOP = STATIC_BASE + 152128;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 152656;
+      return 152992;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
