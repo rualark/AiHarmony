24515ab9b988e7a2c0445419bf9c64c6799fb019
Rualark <rualark@gmail.com>
2020-09-28 01:14:29 +0300

    CA3: Allowed infinite deviation before compensating 4th and 5th leaps. Also allowed to return multiple times to the last note of the leap.

--- docs/prod/js/analysis/modules/CA3.js
+++ docs/prod/js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5396336,
+    STACK_BASE = 5396256,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 153456,
-    DYNAMIC_BASE = 5396336,
-    DYNAMICTOP_PTR = 153296;
+    STACK_MAX = 153376,
+    DYNAMIC_BASE = 5396256,
+    DYNAMICTOP_PTR = 153216;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 152432;
+// STATICTOP = STATIC_BASE + 152352;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 153296;
+      return 153216;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
