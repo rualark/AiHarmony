dfed19b6d3e582f31d33e00233c66e3227b71011
Rualark <rualark@gmail.com>
2020-10-04 16:29:35 +0300

    CA3: Remove all visualization and test data for rules (now data sheet can be cleared if needed), because it is not used in AiHarmony. Migrate CP2D to new leap rule ids.

--- js/analysis/modules/CA3.js
+++ js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5405952,
+    STACK_BASE = 5400784,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 163072,
-    DYNAMIC_BASE = 5405952,
-    DYNAMICTOP_PTR = 162912;
+    STACK_MAX = 157904,
+    DYNAMIC_BASE = 5400784,
+    DYNAMICTOP_PTR = 157744;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 162048;
+// STATICTOP = STATIC_BASE + 156880;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 162912;
+      return 157744;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
