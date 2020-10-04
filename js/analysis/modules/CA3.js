625c2fb6f6909bead930c18d45b2c375ca376841
Rualark <rualark@gmail.com>
2020-10-04 21:44:06 +0300

    CA3. Show less rhythm mistakes. Do not stop evaluating on mistake.

--- js/analysis/modules/CA3.js
+++ js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5400784,
+    STACK_BASE = 5400944,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 157904,
-    DYNAMIC_BASE = 5400784,
-    DYNAMICTOP_PTR = 157744;
+    STACK_MAX = 158064,
+    DYNAMIC_BASE = 5400944,
+    DYNAMICTOP_PTR = 157904;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 156880;
+// STATICTOP = STATIC_BASE + 157040;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 157744;
+      return 157904;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
