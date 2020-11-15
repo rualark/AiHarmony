4f43463c660d098ff77f782af29584e479791e72
Rualark <rualark@gmail.com>
2020-11-15 20:34:00 +0300

    Deploy prod

--- docs/prod/js/analysis/modules/CA3.js
+++ docs/prod/js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5402960,
+    STACK_BASE = 5403040,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 160080,
-    DYNAMIC_BASE = 5402960,
-    DYNAMICTOP_PTR = 159920;
+    STACK_MAX = 160160,
+    DYNAMIC_BASE = 5403040,
+    DYNAMICTOP_PTR = 160000;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 159056;
+// STATICTOP = STATIC_BASE + 159136;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 159920;
+      return 160000;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
