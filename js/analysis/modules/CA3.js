44296dbae69bddaa0ad411cceb02fd4b57d5d134
Rualark <rualark@gmail.com>
2020-11-23 00:50:59 +0300

    Rebuild

--- js/analysis/modules/CA3.js
+++ js/analysis/modules/CA3.js
@@ -1184,11 +1184,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5402752,
+    STACK_BASE = 5402736,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 159872,
-    DYNAMIC_BASE = 5402752,
-    DYNAMICTOP_PTR = 159712;
+    STACK_MAX = 159856,
+    DYNAMIC_BASE = 5402736,
+    DYNAMICTOP_PTR = 159696;
 
 
 
@@ -1603,7 +1603,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 158848;
+// STATICTOP = STATIC_BASE + 158832;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -4159,7 +4159,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 159712;
+      return 159696;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
