2036c0ff41dc1e8c498813c9f1a714f283b3211a
Rualark <rualark@gmail.com>
2020-12-16 01:46:39 +0300

    Start playback from selection

--- js/audio/audio.js
+++ js/audio/audio.js
@@ -1,4 +1,4 @@
-import {abcjs} from "../abc/abchelper.js";
+import {abcjs, selected, state} from "../abc/abchelper.js";
 import { d2midi } from "../notes/noteHelper.js";
 import { nd } from "../notes/NotesData.js";
 import { settings } from "../state/settings.js";
@@ -12,11 +12,33 @@ function setPlayIcon(img) {
   document.getElementById("playi").src = img;
 }
 
+function seekToSelection() {
+  if (state.state !== 'ready') return;
+  if (!selected.element || !selected.element.duration) return;
+  const el = nd.abc_charStarts[selected.element.startChar];
+  if (!el) return;
+  const notes = nd.voices[el.voice].notes;
+  const note = notes[el.note];
+  /*
+  const m = Math.floor(note.step / nd.timesig.measure_len);
+  const last_m = Math.floor(notes[notes.length - 1].step / nd.timesig.measure_len) + 1;
+  console.log('Seek', m/last_m, m, last_m, note, notes);
+  play_state.synth.seek(m / last_m);
+  */
+  const s = note.step;
+  const last_s = notes[notes.length - 1].step + notes[notes.length - 1].len;
+  play_state.synth.seek(s / last_s);
+}
+
+function stop() {
+  play_state.state = 'stopped';
+  setPlayIcon('img/toolbar/play.png');
+  play_state.synth.stop();
+}
+
 export function play() {
   if (play_state.state === 'playing') {
-    play_state.state = 'stopped';
-    setPlayIcon('img/toolbar/play.png');
-    play_state.synth.stop();
+    stop();
   } else {
     play_state.synth = new ABCJS.synth.CreateSynth();
     let AudioContext = window.AudioContext          // Default
@@ -25,9 +47,13 @@ export function play() {
     play_state.synth.init({
       audioContext: myContext,
       visualObj: abcjs[0],
+      options: {
+        onEnded: stop
+      }
     }).then(() => {
       play_state.synth.prime(() => {
       }).then(() => {
+        seekToSelection();
         play_state.synth.start();
         setPlayIcon('img/pause.png');
         play_state.state = 'playing';
