3efcdec4dc2e652d9b2243b911e3ffd37b6eb42f
Rualark <rualark@gmail.com>
2021-01-01 15:39:20 +0300

    Always init sentry, but send errors only in prod

--- js/integration/sentry.js
+++ js/integration/sentry.js
@@ -19,37 +19,39 @@ window.submitSentryUserFeedback = function(message){
   Sentry.captureMessage(message);
 }
 
-if (environment.startsWith('prod')) {
-  Sentry.init({
-    dsn: 'https://ad05883cb9534743b6ca504ece76bba6@sentry.io/1894684',
-    environment: environment,
-    beforeSend(event, hint) {
-      // Check if it is an exception, and if so, show the report dialog
-      let now = new Date();
-      if (event.exception && !debugError && (now - pageLoadTime > 5000)) {
-        alertify.error(`Problem occured and was reported to site administrator. <a style='color: white' href=# onclick='window.showReportDialog("${event.event_id}"); return false;'><b><u>Сlick here</u></b></a> to add details and track this issue.`, 45);
-      }
-      try {
-        event.breadcrumbs.push({
-          timestamp: new Date(),
-          category: 'state',
-          message: urlNoParams() + '?state=' + state2url(),
-          level: Sentry.Severity.Info
-        });
-      }
-      catch (e) {
-        console.log(e);
-      }
-      return event;
+Sentry.init({
+  dsn: 'https://ad05883cb9534743b6ca504ece76bba6@sentry.io/1894684',
+  environment: environment,
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    let now = new Date();
+    if (event.exception && !debugError && (now - pageLoadTime > 5000)) {
+      alertify.error(`Problem occured and was reported to site administrator. <a style='color: white' href=# onclick='window.showReportDialog("${event.event_id}"); return false;'><b><u>Сlick here</u></b></a> to add details and track this issue.`, 45);
     }
-  });
-
-  if (mgen_login) {
-    Sentry.configureScope((scope) => {
-      scope.setUser({
-        "email": mgen_login
+    // Send only in production environment
+    if (event.exception && !environment.startsWith('prod')) {
+      console.warn('Will not send error to sentry in environment', environment);
+      return null;
+    }
+    try {
+      event.breadcrumbs.push({
+        timestamp: new Date(),
+        category: 'state',
+        message: urlNoParams() + '?state=' + state2url(),
+        level: Sentry.Severity.Info
       });
-    });
+    }
+    catch (e) {
+      console.log(e);
+    }
+    return event;
   }
+});
 
+if (mgen_login) {
+  Sentry.configureScope((scope) => {
+    scope.setUser({
+      "email": mgen_login
+    });
+  });
 }
