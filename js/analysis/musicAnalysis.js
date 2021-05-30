5b25b068b097b11aa72ee0f489275e50d6f5f30d
Rualark <rualark@gmail.com>
2021-05-31 00:33:16 +0300

    Support different harmonic notations

--- js/analysis/musicAnalysis.js
+++ js/analysis/musicAnalysis.js
@@ -3,16 +3,9 @@ import {data2plain} from "../state/state.js";
 import {nd} from "../notes/NotesData.js";
 import {ares} from "./AnalysisResults.js";
 import { environment } from "../../js/core/remote.js";
+import { settings } from "../state/settings.js";
 
 export function analyse() {
-  /*
-  let lst = [];
-  for (let i=0; i<200000; ++i) lst.push(i);
-  let tar = new Uint32Array(200000);
-  for (let i=0; i<200000; ++i) tar[i] = i;
-  let st = '';
-  for (let i=0; i<20000; ++i) st += '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789';
-  */
   let debugLevel = 0;
   if (!environment.startsWith('prod')) debugLevel = 10;
   // Prevent drawing old shapes before analysis, while other analysis data should be drawn to prevent flickering
@@ -24,7 +17,8 @@ export function analyse() {
   }
   else {
     launchAnalysis(nd.algo, '__Z7analysePhi', data2plain(), {
-      debugLevel: debugLevel
+      debugLevel: debugLevel,
+      harmNotation: settings.harm_notation
     });
   }
 }
